86d017cfe180135873cde153da0d2febdef90c1c
Suporte <suporte@estagiario>
2018-04-05 16:49:09 -0300

    Python parser finalizado (por enquanto)

--- NmapXML/nmap_parser.py
+++ NmapXML/nmap_parser.py
@@ -1,41 +1,60 @@
 # -*- coding: utf-8 -*-
 import xml.etree.cElementTree as ET
+import json as js
+class port:
+    def __init__(self):
+        self.portId = None
+        self.portService = None
+        self.portState = None
 
-def printJson(arquivo):
+
+def extracaoDados(arquivo):
 	tree = ET.parse(arquivo)
 	root = tree.getroot()
 	ipv4 = None
 	mac = None
-	portId = None
-	portService = None
-	portState = None
 	os = None
 	osAcc = None
 
 
 	#hosts = root.findall('host')
 	for results in root.iterfind('host'):
+		ports = [port()for i in range(len(results.findall('./ports/port')))]
 		for address in results.findall('address'):
-				if(address.get('addrtype')=="ipv4"):
-					ipv4=address.get('addr')
-					print(ipv4)
+				if(address.get('addrtype') == "ipv4"):
+					ipv4 = address.get('addr')
 				else:
 					mac = address.get('addr')
-					print(mac)
 
 		if(results.find('./ports/port')):
-			for ports in results.findall('./ports/port'):
-				portId = ports.get('portid')
-				portState = ports.find('./state').get('state')
-				portService = ports.find('./service').get('name')
-				print(portState)
-
+			i = 0
+			for portas in results.findall('./ports/port'):
+				
+				ports[i].portId = portas.get('portid')
+				ports[i].portState = portas.find('./state').get('state')
+				ports[i].portService = portas.find('./service').get('name')
+				i+=1
 		if(results.find('.os/osmatch')):
 			os = results.find('.os/osmatch').get('name')
 			osAcc = results.find('.os/osmatch').get('accuracy')
-			print(os + " " + osAcc + "%")
+
+	printJson(ports, ipv4, mac, os)
+
+def printJson(ports, ipv4, mac, os):
+	json=('"MAC":"{0}","ipv4":"{1}","OS":"{2}","ports":'.format(mac,ipv4,os))
+	json = json + "{"
+	if(len(ports) == 0):
+		json = json + "}"
+	else:
+
+		for i in range(len(ports)):
+			json =(json +'"{0}":"{1}",'.format(ports[i].portId,ports[i].portService))
+		json = (json[:-1] + "}")
+	json = "{" + json + "}"
+	print(json)
+	return json
 
 
 	#print(root.child)
 
-printJson("/home/emanuel/nmap_file2.xml")
+extracaoDados("/home/suporte/Documentos/GitHub/TCC/NmapXML/nmap_file2.xml")
